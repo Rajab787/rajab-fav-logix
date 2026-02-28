6bc10999eb0a993e1ae78e9deb736dba9f867cfd
Rajab787 <rajab10312@gmail.com>
2026-02-28 23:13:37 +0500

    initial edits

---
+++ next.config.ts
@@ -0,0 +1,26 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dummyjson.com",
+      },
+    ],
+  },
+
+  // Redirects
+  async redirects() {
+    return [
+      {
+        source: '/dashboard',         // user ka requested path
+        destination: '/dashboard/inbox', // redirect target
+        permanent: false,             // false = temporary redirect (307)
+      },
+    ];
+  },
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Banner from "@/components/Banner/banner";
+import ChatBot from "@/components/ChatBox";
+import DetailsPanel from "@/components/ChatDetailPanel";
+import Sidebar from "@/components/sidebar/Sidebar";
+import SingleChatPage from "@/components/SIngleChatPage/SingleChatPage";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+      <Banner />
+    
+    </div>
+  );
+}
